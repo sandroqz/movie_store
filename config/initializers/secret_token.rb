42e8f995c766b9c22baf38fefde3bd81572504a0
Sandro Queiroz Jr <sandroqz@gmail.com>
2013-02-04 07:56:11 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovieStore::Application.config.secret_token = '183e21b6f55bb7efdacfbda77ca06aec48206559dc878a4bf0c38b34a6c2d45c619b130bfa5f28a43036b6fa0c62a14f8bfbe3283d5c4a8a7d01383f66f5dc01'
