e7ec07d8a12c16ec5deda63049c98023e604199b
Sandro Queiroz Jr <sandroqz@gmail.com>
2013-02-04 09:39:28 -0300

    Implementação do modelo User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
+  has_many :shopping_carts
   has_secure_password
+
+  def start_shopping
+    shopping_carts.create total_value: 0, open_date: Date.today
+  end
+  def is_owner?(movie)
+    !movie.owners.where(id: self.id).empty?
+  end
 end
