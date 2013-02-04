f4273017518f14a5970e44afad1fd9522158ddd1
Sandro Queiroz Jr <sandroqz@gmail.com>
2013-02-04 09:16:01 -0300

    Implementação do modelo Movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,12 @@
 class Movie < ActiveRecord::Base
+  has_many :user_movies
+  has_many :owners, through: :user_movies, source: :user
+
+  validates :name, :file_name, :preview_file_name, :description, :stream_price, presence: true
+  validates :stream_price, :download_price, numericality: {greater_than: 0, allow_nil: true}
+  validates :file_name, :preview_file_name, format: {with: /[a-z]{3,4}:\/[a-z0-9_\-.]+(\/[a-z0-9\-_]+)+/i}
+
+  def can_be_downloaded_by?(user)
+    user_movies.where(user_id: user).first.try(:can_download)
+  end
 end
