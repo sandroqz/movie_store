fc0eb7f5165a85311f83662ea0d6ab92bc23bfaa
Sandro Queiroz Jr <sandroqz@gmail.com>
2013-02-04 10:06:23 -0300

    Definindo a has_many movies no model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ActiveRecord::Base
   has_many :shopping_carts
+  has_many :user_movies
+  has_many :movies, through: :user_movies do
+    def streamable
+      where("user_movies.can_download=?", false)
+    end
+    def downloadable
+      where("user_movies.can_download=?", true)
+    end
+  end
+
   has_secure_password
 
   def start_shopping
